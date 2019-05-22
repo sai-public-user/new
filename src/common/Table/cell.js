82c8a7a01df1096213ccdc7cb1bf08890a2ae22c
sai-public-user <Saimanideep@hotmail.com>
2019-05-21 20:30:51 -0400

    Added toggle switch for 30 and 90 days

--- src/common/Table/cell.js
+++ src/common/Table/cell.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react';
+import * as Styles from './SharedStyles';
+
+const {
+    TableRowCell,
+} = Styles.default;
 
 function Cell(props) {
     const { data = '', isPinnedVal = false, endPosition = -1 } = props;
     return (
-        <div className={`row table_cell col m-0 p-0 pl-2 align-content-center${isPinnedVal ? ' pinned_cell' : ''}${endPosition > 0 ? ' last_cell' : ''}`} name={data}>
+        <TableRowCell className={`${isPinnedVal ? ' pinned_cell' : ''}${endPosition > 0 ? ' last_cell' : ''}`} name={data}>
             {data}
-        </div>
+        </TableRowCell>
     )
 }
 
--- src/common/Table/row.js
+++ src/common/Table/row.js
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react';
 import Cell from './cell';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import * as Styles from './SharedStyles';
 
-// import * as Styles from './SharedStyles';
-
-// const {
-    
-// } = Styles.default;
+const {
+    RowCheckbox,
+    FirstCell,
+    TableRow,
+} = Styles.default;
 
 function Row(props) {
     const {
@@ -16,14 +17,14 @@ function Row(props) {
     } = props;
     const headerVals = Array.isArray(headers) && headers.map(({ value ='' }) => value) || [];
     return (
-        <div className="table_row text-capitalize row mt-3 ml-1 mr-1">
-            <div className="row table_cell  m-0 p-2 align-content-center row-checkbox">
-                <span className="first_cell"><Checkbox checked={checked.includes(row.id)} onChange={checkBoxChange} name={row.id} /></span>
-            </div>
+        <TableRow>
+            <RowCheckbox>
+                <FirstCell><Checkbox checked={checked.includes(row.id)} onChange={checkBoxChange} name={row.id} /></FirstCell>
+            </RowCheckbox>
             {row && row !== null && Array.isArray(headerVals) && headerVals.map((keyVal, i) => (
                 <Cell data={row[keyVal]} isPinnedVal={pinned.includes(keyVal)} endPosition={ i === 0 || i === headerVals.length - 1 ? i : -1 } />
             ))}
-        </div>
+        </TableRow>
     );
 }
 
